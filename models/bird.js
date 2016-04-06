9b67be40038f0fd9f61deb4019ad4303bc79c5fd
Brittany <lotuxi@me.com>
2016-04-05 22:09:23 -0500

    BirdSpotting
    BirdSpotting web app for class

---
+++ models/bird.js
@@ -0,0 +1,18 @@
+var mongoose = require("mongoose")
+var Schema = mongoose.Schema;
+
+//bird watcher database
+var birdSchema = Schema({
+  name : { type : String,
+    required : true,
+    unique : true,
+    lowercase : true },
+  description : String,
+  averageEggsLaid : {type : Number, min : 1, max : 50},
+  threatened : { type : Boolean, default : false},
+  datesSeen : [{ type : Date, default : Date.now}]
+});
+
+var Bird = mongoose.model('Bird', birdSchema);
+
+module.exports = Bird;
---
+++ routes/index.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var router = express.Router();
+
+var Bird = require('../models/bird.js');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  Bird.find(function(err, birdDocs){
+    if (err) { return next(err); }
+    return res.render('index', { birds: birdDocs , error : req.flash('error') });
+  });
+});
+
+router.post('/', function(req, res, next) {
+
+  for (var att in req.body) {
+    if (req.body[att] === '') {
+      delete(req.body[att])
+    }
+  }
+  console.log(req.body.datesSeen)
+  var date = req.body.datesSeen || Date.now() ;
+
+  req.body.datesSeen = [];
+  req.body.datesSeen.push(date)
+
+
+  //create new birds
+  var newSighting = Bird(req.body);
+
+  //request that is save.
+  newSighting.save(function(err){
+    if (err) {
+      if (err.name == "ValidationError") {
+        req.flash('error', 'Invalid data');
+        return res.redirect('/')
+      }
+
+      if (err.code == 11000) {
+        req.flash('error', 'A bird with that name already exisits')
+        return res.redirect('/')
+      }
+      return next(err);
+    }
+    res.status(201);
+    return res.redirect('/');
+  });
+});
+
+router.post('/addDate', function(req, res, next) {
+
+  var newSighting = req.body.datesSeen
+  if (!newSighting || newSighting == '') {
+    return res.redirect('/');
+  }
+
+  Bird.findOne( {name : req.body.name }, function( err, bird) {
+    if (err) { return next(err) }
+    if (!bird) { return next(new Error('No bird found with name ' + req.body.name))}
+
+    bird.datesSeen.push(newSighting);
+
+    bird.save(function(err) {
+      if (err) { return next(err); }
+      res.redirect('/')
+    });
+  });
+});
+
+module.exports = router;
